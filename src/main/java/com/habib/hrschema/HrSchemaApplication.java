65728f3c14d1ec863077e12449fa8892a36c8f93
Habib <habib.alhabib@esprit.tn>
2023-07-12 23:22:26 +0200

    First commit

---
+++ src/main/java/com/habib/hrschema/HrSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.habib.hrschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habib/hrschema/HrSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habib.hrschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
